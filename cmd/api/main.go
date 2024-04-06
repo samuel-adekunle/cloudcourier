7841bfd464fd18e52ef4efce0083b4fa0ddc867c
Oyetunji Ibukunoluwa Oluwole <78256143+Ibukun-tech@users.noreply.github.com>
2024-04-06 22:14:30 +0100

    feat:still working on the cloudinary services

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Ibukun-tech/cloudcourier"
-)
-
 func main() {
-	s := &cloudcourier.CloudCourierBridge{
-		CloudProvider: cloudcourier.CloudinaryServices,
-		ApiKey:        "KSKSKSK",
-		ApiSecret:     "KJJSJSJSJ",
-		CloudName:     "Ibukun first Name",
-	}
 
-	t, _ := cloudcourier.NewCloudCourier(s)
-	fmt.Println(t.UploadFile(&cloudcourier.File{}))
 }
